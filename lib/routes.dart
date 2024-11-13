568d1333f4d9b85eb6e3699b320f19e5f9555a20
daoxuantri <thegioithangkhung@gmail.com>
2024-11-13 18:04:08 +0700

    upload route screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,6 @@
-import 'dart:js';
-
 import 'package:ecommerce_app_mobile/components_buttons/bottom_navbar_home.dart';
 import 'package:ecommerce_app_mobile/screens/all_product/all_product_screen.dart';
+import 'package:ecommerce_app_mobile/screens/category/my_category.dart';
 import 'package:ecommerce_app_mobile/screens/forget_password/components/email_password/form_email.dart';
 import 'package:ecommerce_app_mobile/screens/forget_password/components/email_password/form_id_email/id_email.dart';
 import 'package:ecommerce_app_mobile/screens/forget_password/components/reset_password/reset_pass.dart';
@@ -25,18 +24,25 @@ final Map<String , WidgetBuilder> routes ={
   //signup
   SignUpScreen.routeName :(context) => const SignUpScreen(),
 
+   /*-------------------Dieu huong bottom navigation bar--------------------------*/
+   
   //Navigator Bottom bar
   NavigatorBottomBarHome.routeName : (context) => const NavigatorBottomBarHome(),
 
+  //home
+  HomeScreen.routeName : (context) => const HomeScreen(),
+  //category
+  CategoryScreen.routeName :(context) => const CategoryScreen(),
 
-//form email pass
+  /*-----------------------------------------------------------------------------*/
+
+  //form email pass
   // FormEmailPass.routeName : (context) => const FormEmailPass(),
   IDEmail.routeName : (context) => const IDEmail(),
 
   //respass
   ResetPassScreen.routeName : (context) => const ResetPassScreen(),
-  //home
-  HomeScreen.routeName : (context) => const HomeScreen(),
+  
   //all product screen
   AllProductScreen.routeName :(context) => const AllProductScreen(),
 
